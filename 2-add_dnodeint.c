46cd7487997f14cd749a822a0e1f0a13b1270652
Your Name <yohel.cruz.espinoza@gmail.com>
2022-05-11 16:07:06 -0700

    Added 2-add_dnodeint.c

---
+++ 2-add_dnodeint.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * add_dnodeint - Add a node at the head of the linked list.
+ *
+ * @head: Double pointer to the linked list.
+ *
+ * @n: Value of the new node.
+ *
+ * Return: Pointer to the new node, if fails return NULL
+ */
+
+stack_t *add_dnodeint(stack_t **head, const int n)
+{
+	stack_t *new_node = NULL;
+
+	if (!head)
+		return (NULL);
+
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->prev = NULL;
+	new_node->n = n;
+
+	if (*head)
+	{
+		new_node->next = *head;
+		(*head)->prev = new_node;
+	}
+
+	*head = new_node;
+	return (*head);
+}
