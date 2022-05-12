62a4f8c58f09cd8c5224fba68306ee00903ae2e8
WardenCode <diegojeanluck@hotmail.com>
2022-05-12 00:39:21 -0500

    fixing betty style

--- advanced_opcodes.c
+++ advanced_opcodes.c
@@ -129,3 +129,32 @@ void mod(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 }
+
+/**
+ * pchar - function that prints the char at the top of the stack.
+ * @stack: Double pointer to stack.
+ * @line_number: Number of line of the monty file.
+ *
+ * Return: void
+ */
+
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+
+	if (*stack && global.quantity >= 1)
+	{
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+
+		tmp->prev->n *= tmp->n;
+		pop(stack, line_number);
+	}
+	if (tmp->n > 127 || tmp->n < 0)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't pchar, value out of range\n",
+				line_number);
+		free_cases(0);
+		exit(EXIT_FAILURE);
+	}
+}
