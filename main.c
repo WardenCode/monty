14f14f10c2a1a4cd2ac22935cf01c8e983d9c114
WardenCode <diegojeanluck@hotmail.com>
2022-05-12 02:12:19 -0500

    handling malloc error of tokenizer

--- main.c
+++ main.c
@@ -60,39 +60,39 @@ int main(int ac, char **av)
 	void (*op_func)(stack_t **, unsigned int);
 
 	if (ac != 2)
-	{
-		dprintf(STDERR_FILENO, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-
+		dprintf(STDERR_FILENO, "USAGE: monty file\n"), exit(EXIT_FAILURE);
 	if (access(av[1], R_OK) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
-
 	global.fd_monty = fopen(av[1], "r");
 	while (getline(&global.command, &len, global.fd_monty) != EOF)
 	{
 		if (global.command[0] == '\n' || is_comment(global.command) ||
 			all_spaces(global.command, strlen(global.command)))
 		{
-			global.line_num++, len = 0;
-			free(global.command);
+			global.line_num++, len = 0, free(global.command);
 			continue;
 		}
 		global.tokens = tokenizer(global.command);
+		if (!global.tokens)
+		{
+			dprintf(STDERR_FILENO, "Error: malloc failed\n");
+			/*free(global.command);*/
+			/*free_dlistint(global.stack);*/
+			/*fclose(global.fd_monty);*/
+			free_cases(0), exit(EXIT_FAILURE);
+		}
 		op_func = choose_option(global.tokens);
 		if (op_func == NULL)
 		{
 			dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n",
 					global.line_num, global.tokens[0]);
-			free_cases(0);
-			exit(EXIT_FAILURE);
+			free_cases(0), exit(EXIT_FAILURE);
 		}
 		op_func(&global.stack, global.line_num);
-		free_cases(2);
-		global.line_num++, len = 0;
+		free_cases(2), global.line_num++, len = 0;
 	}
 	free_cases(1);
 	return (EXIT_SUCCESS);
