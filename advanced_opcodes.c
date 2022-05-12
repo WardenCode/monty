7e4eaf4c96e82e6f1f32e3a6acb80d1caa0a6d07
Your Name <yohel.cruz.espinoza@gmail.com>
2022-05-11 22:23:33 -0700

    Added functions mul & mod

--- advanced_opcodes.c
+++ advanced_opcodes.c
@@ -64,3 +64,68 @@ void _div(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 }
+
+/**
+ * mul - function that multiplies the top 2 elements of the stack.
+ * @stack: Double pointer to stack.
+ * @line_number: Number of line of the monty file.
+ *
+ * Return: void
+ */
+
+void mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+
+	if (*stack && global.quantity >= 1)
+	{
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+
+		tmp->prev->n *= tmp->n;
+		pop(stack, line_number);
+	}
+	else
+	{
+		dprintf(STDERR_FILENO, "L%u: can't mul, stack too short\n", line_number);
+		free_cases(0);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * mod - computes the rest of the division of the second top element
+ * of the stack by the top element of the stack.
+ * @stack: Double pointer to stack.
+ * @line_number: Number of line of the monty file.
+ *
+ * Return: void
+ */
+
+void mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+
+	if (*stack && global.quantity >= 1)
+	{
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		if (tmp->n == 0)
+		{
+			dprintf(STDERR_FILENO, "L%u: division by zero\n", line_number);
+			free_cases(0);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			tmp->prev->n %= tmp->n;
+			pop(stack, line_number);
+		}
+	}
+	else
+	{
+		dprintf(STDERR_FILENO, "L%u: can't mod, stack too short\n", line_number);
+		free_cases(0);
+		exit(EXIT_FAILURE);
+	}
+}
