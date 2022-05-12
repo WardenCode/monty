b41aa95db69ca757b513a5d1f85790245511f48a
WardenCode <diegojeanluck@hotmail.com>
2022-05-12 01:09:26 -0500

    Fixing typos on pchar function

--- advanced_opcodes.c
+++ advanced_opcodes.c
@@ -141,7 +141,6 @@ void mod(stack_t **stack, unsigned int line_number)
 void pchar(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = *stack;
-	char c;
 
 	if (*stack && global.quantity >= 1)
 	{
@@ -150,18 +149,16 @@ void pchar(stack_t **stack, unsigned int line_number)
 
 		if (tmp->n > 127 || tmp->n < 0)
 		{
-			dprintf(STDERR_FILENO, "L%d: can't pchar, value out of range\n",
+			dprintf(STDERR_FILENO, "L%u: can't pchar, value out of range\n",
 					line_number);
 			free_cases(0);
 			exit(EXIT_FAILURE);
 		}
-		c = tmp->n;
-		printf("%c\n", c);
-		tmp = tmp->prev;
+		printf("%c\n", tmp->n);
 	}
 	else
 	{
-		dprintf(STDERR_FILENO, "L%d: can't pchar, stack empty\n", line_number);
+		dprintf(STDERR_FILENO, "L%u: can't pchar, stack empty\n", line_number);
 		free_cases(0);
 		exit(EXIT_FAILURE);
 	}
